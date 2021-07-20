8dda6e7c7a89a5a6d783b28505f8e64d3efd6fe4
Marwane <marwane.benoukaiss@wiilog.fr>
2021-07-20 09:25:32 +0200

    Add scan service

---
+++ src/app/services/scanner.service.ts
@@ -0,0 +1,46 @@
+import {Injectable} from '@angular/core';
+import {Plugins} from '@capacitor/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ScannerService {
+
+    private body;
+
+    constructor() {
+    }
+
+    public scan(success = null, failure = null) {
+        const {BarcodeScanner} = Plugins;
+
+        this.show();
+        BarcodeScanner.startScan().then(result => {
+            this.hide();
+
+            if (result.hasContent && success != null) {
+                success(result.content);
+            } else if(!result.hasContent && failure != null) {
+                failure();
+            }
+        });
+    }
+
+    public hide() {
+        this.setOpacity(1);
+    }
+
+    public show() {
+        this.setOpacity(0.5);
+    }
+
+    setOpacity(value: number) {
+        if (!this.body) {
+            this.body = document.getElementsByTagName(`html`)[0];
+        }
+
+        this.body.style.transition = `opacity 500ms`;
+        this.body.style.opacity = `${value}`;
+    }
+
+}
